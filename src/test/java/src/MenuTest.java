a31db04f827427333121465aa479e8aef92a4ba4
natalia <255573@edu.p.lodz.pl>
2026-06-09 19:12:05 +0200

    added MenuTest.java with input validation tests.

---
+++ src/test/java/src/MenuTest.java
@@ -0,0 +1,44 @@
+package src;
+
+import org.junit.jupiter.api.Test;
+import src.main.Menu;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MenuTest {
+    @Test
+    public void poprawnyWyborGraj() {
+        Menu menu = new Menu();
+        assertEquals(1, menu.parseChoice("1"));
+    }
+
+    @Test
+    public void poprawnyWyborZarzadzanieSlowami() {
+        Menu menu = new Menu();
+        assertEquals(2, menu.parseChoice("2"));
+    }
+
+    @Test
+    public void poprawnyWyborWyjscie() {
+        Menu menu = new Menu();
+        assertEquals(3, menu.parseChoice("3"));
+    }
+
+    @Test
+    public void blednyWyborLitera() {
+        Menu menu = new Menu();
+        assertEquals(-1, menu.parseChoice("X"));
+    }
+
+    @Test
+    public void blednyWyborPozaZakresem() {
+        Menu menu = new Menu();
+        assertEquals(-1, menu.parseChoice("5"));
+    }
+
+    @Test
+    public void blednyWyborPustePole() {
+        Menu menu = new Menu();
+        assertEquals(-1, menu.parseChoice(""));
+    }
+}
