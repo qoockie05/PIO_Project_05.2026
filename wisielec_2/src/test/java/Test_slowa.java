774488309f3bd24a9847eb6271833668ecda3bb8
alicjajaros <255613@edu.p.lodz.pl>
2026-06-11 13:20:43 +0200

    Dodanie testow jednostkowych dla klas Gracz i Slowa

---
+++ wisielec_2/src/test/java/Test_slowa.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test_slowa {
+
+    @Test
+    void powinnoLosowacSlowoZListy() {
+        Slowa slowa = new Slowa();
+        List<String> dozwoloneSlowa = Arrays.asList(
+                "komputer",
+                "program",
+                "java",
+                "wisielec",
+                "terminal",
+                "klawiatura",
+                "projekt",
+                "student",
+                "uczelnia",
+                "algorytm"
+        );
+
+        String wynik = slowa.losujSlowo();
+        assertTrue(dozwoloneSlowa.contains(wynik));
+    }
+
+    @Test
+    void wylosowaneSlowoNiePowinnoBycNullem() {
+        Slowa slowa = new Slowa();
+        String wynik = slowa.losujSlowo();
+        assertNotNull(wynik);
+    }
+
+    @Test
+    void wylosowaneSlowoNiePowinnoBycPuste() {
+        Slowa slowa = new Slowa();
+        String wynik = slowa.losujSlowo();
+        assertFalse(wynik.isEmpty());
+    }
+}
